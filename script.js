69bdaca569d23eb48c42f2b7f6010747525542b4
Teiko <teikomancini1993@gmail.com>
2020-10-26 13:30:04 +0100

    not working

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-/*rollTheDice= () => {
+/*when movePlayer() works do newPosition=newPosition+rollTheDice (create return)
+
+rollTheDice= () => {
     let diceNumber = Math.round(Math.random()*5)+1
     console.log(diceNumber);
 }*/
+let boardboxArray = [
+    "box-1",
+    "box-2",
+    "box-3",
+    "box-4",
+    "box-5",
+    "box-6",
+    "box-7",
+    "box-8",
+    "box-9",
+    "box-10"
+]
+let oldPosition =0
+let newPosition =0
+
+document.getElementById("button").addEventListener("click", function movePlayer() {
+    oldPosition = newPosition;
+    newPosition++;
+    document.getElementById(boardboxArray[oldPosition]).removeChild
+    document.getElementById(boardboxArray[newPosition]).appendChild(/*what to write*/)
+  });
