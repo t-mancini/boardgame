8add60db2e92e7178c216496e877157faeb577d6
Teiko <teikomancini1993@gmail.com>
2020-10-26 19:04:32 +0100

    i'm a fucking geniusgit add .!

--- script.js
+++ script.js
@@ -25,93 +25,64 @@ document.getElementById("button").addEventListener("click", function(){
     document.getElementById(boardArray[oldPosition]).removeChild(document.getElementById("player-one"));
     });
 
-const players = {
-    player1: {
+
+const players = [
+    {
         id: "player-one",
         class: "player",
         oldPosition: 0,
         newPosition:0,
         color:"red"},
-    player2: {
+    {
         id: "player-two",
         class: "player",
         oldPosition: 0,
         newPosition:0,
         color:"blue"},
-    player3: {
+    {
         id: "player-three",
         class: "player",
         oldPosition: 0,
         newPosition:0,
         color:"green",},
-    player4: {
+    {
         id: "player-four",
         class: "player",
         oldPosition: 0,
         newPosition:0,
         color:"yellow"}
-}
+    ]
 
     /*works, i will finish it after creating the players object}*/
     /*i need to delete the create player button, to prevent multiple clicks*/
    document.getElementById("selectPlayers").addEventListener("click", function(){
     switch (document.getElementById("player-number").value) {
-    case "2":
-        playerOne = document.createElement("DIV");
-        playerOne.setAttribute("id", players.player1.id);
-        playerOne.setAttribute("class", players.player1.class);
-        document.getElementById(boardArray[0]).appendChild(playerOne);
-        document.getElementById("player-one").style.backgroundColor = players.player1.color
-
-        playerTwo = document.createElement("DIV");
-        playerTwo.setAttribute("id", players.player2.id);
-        playerTwo.setAttribute("class", players.player2.class);
-        document.getElementById(boardArray[0]).appendChild(playerTwo);
-        document.getElementById("player-two").style.backgroundColor = players.player2.color
-    break;
+        case "2":
+            for (i = 0; i < 2; i++) {
+                playerOne = document.createElement("DIV");
+                playerOne.setAttribute("id", players[i].id);
+                playerOne.setAttribute("class", players[i].class);
+                document.getElementById(boardArray[0]).appendChild(playerOne);
+                document.getElementById(players[i].id).style.backgroundColor = players[i].color 
+            }
+        break;
   case "3":
-    playerOne = document.createElement("DIV");
-    playerOne.setAttribute("id", players.player1.id);
-    playerOne.setAttribute("class", players.player1.class);
-    document.getElementById(boardArray[0]).appendChild(playerOne);
-    document.getElementById("player-one").style.backgroundColor = players.player1.color
-
-    playerTwo = document.createElement("DIV");
-    playerTwo.setAttribute("id", players.player2.id);
-    playerTwo.setAttribute("class", players.player2.class);
-    document.getElementById(boardArray[0]).appendChild(playerTwo);
-    document.getElementById("player-two").style.backgroundColor = players.player2.color
-
-    playerThree = document.createElement("DIV");
-    playerThree.setAttribute("id", players.player3.id);
-    playerThree.setAttribute("class", players.player3.class);
-    document.getElementById(boardArray[0]).appendChild(playerThree);
-    document.getElementById("player-three").style.backgroundColor = players.player3.color
+    for (i = 0; i < 3; i++) {
+        playerOne = document.createElement("DIV");
+        playerOne.setAttribute("id", players[i].id);
+        playerOne.setAttribute("class", players[i].class);
+        document.getElementById(boardArray[0]).appendChild(playerOne);
+        document.getElementById(players[i].id).style.backgroundColor = players[i].color 
+    }
     break;
   case "4":
-    playerOne = document.createElement("DIV");
-    playerOne.setAttribute("id", players.player1.id);
-    playerOne.setAttribute("class", players.player1.class);
-    document.getElementById(boardArray[0]).appendChild(playerOne);
-    document.getElementById("player-one").style.backgroundColor = players.player1.color
-
-    playerTwo = document.createElement("DIV");
-    playerTwo.setAttribute("id", players.player2.id);
-    playerTwo.setAttribute("class", players.player2.class);
-    document.getElementById(boardArray[0]).appendChild(playerTwo);
-    document.getElementById("player-two").style.backgroundColor = players.player2.color
-
-    playerThree = document.createElement("DIV");
-    playerThree.setAttribute("id", players.player3.id);
-    playerThree.setAttribute("class", players.player3.class);
-    document.getElementById(boardArray[0]).appendChild(playerThree);
-    document.getElementById("player-three").style.backgroundColor = players.player3.color
-
-    playerFour = document.createElement("DIV");
-    playerFour.setAttribute("id", players.player4.id);
-    playerFour.setAttribute("class", players.player4.class);
-    document.getElementById(boardArray[0]).appendChild(playerFour);
-    document.getElementById("player-four").style.backgroundColor = players.player4.color
+    for (i = 0; i < 4; i++) {
+        playerOne = document.createElement("DIV");
+        playerOne.setAttribute("id", players[i].id);
+        playerOne.setAttribute("class", players[i].class);
+        document.getElementById(boardArray[0]).appendChild(playerOne);
+        document.getElementById(players[i].id).style.backgroundColor = players[i].color 
+    }
     break;
     }
     });
