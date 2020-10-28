88f012bc55c71c98af728d41f4a5006341ed0558
Teiko <teikomancini1993@gmail.com>
2020-10-28 14:43:35 +0100

    working. now want a -whatisyourname- form

--- script.js
+++ script.js
@@ -47,33 +47,36 @@ document.getElementById("selectPlayers").addEventListener("click", function(){
     }
     });
 
-/*move players*/    /*ERROR:players length gives me 4 and not the spawned players*/
+/*move players*/
 let j=0
 document.getElementById("button").addEventListener("click", function(){
     const increment = Math.round(Math.random()*5)+1;
     if(j==i){j=0}
+    /*if win this happens*/
+    if(players[j].position+increment>boardArray.length){
+        /*you won alert+button*/
+        document.getElementById("finish").style.display = "block"
+        document.getElementById("button").style.display = "none"
+        document.getElementById("finish").innerHTML=`<p id="winningPlayer">Player ${players[j].id} won!</p>
+        <button type="button" id="restart">Start again</button>`
+    }
+    /*if not win this happens*/
+    else{
     document.getElementById(boardArray[players[j].position+increment]).appendChild(document.getElementById(players[j].id));
     players[j].position=players[j].position+increment;
-    j++
-    }    
+    j++}
+    /*make game restart=page reload*/
+    document.getElementById("restart").addEventListener("click", function(){
+        window.location.href = window.location.href;
+    });
+  }  
 );
 
-/* for(i=0; i<=players.length; i++){
-    document.getElementById(boardArray[players[i].position+increment]).appendChild(document.getElementById(players[i].id));
-    players[i].position=players[i].position+increment;
-    } */
-
-/* document.getElementById("button").addEventListener("click", function(){
-    const increment = rollTheDice()
-    document.getElementById(boardArray[players[0].position+increment]).appendChild(document.getElementById(players[0].id));
-    players[0].position=players[0].position+increment;
-    }
-        
-); */
-
-
-
-
-/*i need a "when you finish the array you won"function*/
-
-    /*i need a start again button when everything ends, and i need div id="init" to be shown again*/
+/*TO DO:
+-show me "player 1 is playing"
+-see the dice
+-keyframe animation to append when player moves
+-give name to player, message when someone finish
+-want arrays outside
+-extra array for event cards (?)
+*/
