26d7b1ad41f2299e8bc693b747680803a4b2afc5
Teiko <teikomancini1993@gmail.com>
2020-11-02 17:18:56 +0100

    first merge working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let boardArray = [
+/* let boardArray = [
     "box-1",
     "box-2",
     "box-3",
@@ -40,9 +40,9 @@ let players = [
         class: "player",
         position:0,
         color:"yellow"}
-    ]
+    ] */
 /*create new players*/ /*hides initialize, show nameChoiceForm*/
-document.getElementById("selectPlayers").addEventListener("click", function(){
+/* document.getElementById("selectPlayers").addEventListener("click", function(){
     switch (document.getElementById("player-number").value) {
         case document.getElementById("player-number").value:
             document.getElementById("init").style.display = 'none';
@@ -60,12 +60,12 @@ document.getElementById("selectPlayers").addEventListener("click", function(){
             }
         break;
     }
-    });
+    }); */
     /*save the names inside the object*/
     /*hides give me your name*/
     /*show move players button*/
     /*initialize player1 is playing text*/
-document.getElementById("setName").addEventListener("click", function(){
+/* document.getElementById("setName").addEventListener("click", function(){
     document.getElementById("setName").style.display = 'none';
     document.getElementById("nameChoice").style.display = 'none';
     document.getElementById("button").style.display = 'block';
@@ -73,22 +73,80 @@ document.getElementById("setName").addEventListener("click", function(){
     for (i = 0; i < document.getElementById("player-number").value; i++) {
         players[i].name=document.getElementById(players[i].idName).value
     }
-    document.getElementById("whoIsPlaying").innerHTML=`<p>${players[0].name}, your turn</p>`})
+    document.getElementById("whoIsPlaying").innerHTML=`<p>${players[0].name}, your turn</p>`}) */
+
+    /*new script*/
+
+    let boardArray = [
+        "box-1",
+        "box-2",
+        "box-3",
+        "box-4",
+        "box-5",
+        "box-6",
+        "box-7",
+        "box-8",
+        "box-9",
+        "box-10"
+    ]
+    let keyNumber = []
+    let keyId = []
+    let keyColor =[]
+    let createPlayer = ""
+    let createForm = ""
+    let keyName = []
+    let players = []
+    /* first loop. create form and thats all */
+    document.getElementById("selectPlayers").addEventListener("click", function(){
+    for(let i=0; i<=document.getElementById("player-number").value-1;i++){
+        createForm += `<form>
+        <label for="${keyNumber[i]}">Player ${i+1}:</label><br>
+        <input type="text" id="${keyNumber[i]}" name="${keyNumber[i]}"><br>
+    </form>`
+    }
+    /*remove some stuff and shows me other stuff*/
+    document.getElementById("nameChoice").innerHTML=createForm
+    document.getElementById("init").style.display = 'none';
+    document.getElementById("setName").style.display = 'block';
+    })
+    /*creates name array*/ /*inside the same for loop create the players array*/
+    document.getElementById("setName").addEventListener("click", function(){
+        for(let i=0;i<document.getElementById("player-number").value; i++){
+            keyNumber.push(i+1)
+            keyId.push(`player-${keyNumber[i]}`)
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            keyColor.push("#"+randomColor)
+            /* give the div a id so that it can getelementbyid */
+            document.getElementsByTagName("input")[i].id=keyNumber[i]
+            keyName.push(document.getElementById(keyNumber[i]).value)
+            /*i dont need the players array anymore, have all the data stored??*/
+            player = {"number":keyNumber[i],"id":keyId[i],"name":keyName[i],"class":"player","position":0,"color":keyColor[i]}
+            players.push(player)
+            createPlayer += `<div id="${keyId[i]}" class="player" style="background-color:${keyColor[i]}"></div>`
+        }
+        /*delete some stuff and show some stuff*/
+        document.getElementById(boardArray[0]).innerHTML=createPlayer
+        document.getElementById("setName").style.display = 'none';
+        document.getElementById("nameChoice").style.display = 'none';
+        document.getElementById("button").style.display = 'block';
+        document.getElementById("whoIsPlaying").style.display = 'block';
+        document.getElementById("whoIsPlaying").innerHTML=`<p>${keyName[0]}, your turn</p>`
+    })
 
 /*move players*/
-let j=0
+let i=0
 document.getElementById("button").addEventListener("click", function(){
     const increment = Math.round(Math.random()*5)+1;
-    if(j==i){j=0}
+    if(i==players.length){i=0}
     /*if win this happens*/
-    if(players[j].position+increment>boardArray.length){
+    if(players[i].position+increment>boardArray.length){
         /*you won alert+button*/
 
         document.getElementById("finish").style.display = "block"
         document.getElementById("button").style.display = "none"
-        document.getElementById("finish").innerHTML=`<p id="winningPlayer">${players[j].name} won!</p>
+        document.getElementById("finish").innerHTML=`<p id="winningPlayer">${players[i].name} won!</p>
         <button type="button" id="restart">Start again</button>`
-        document.getElementById(boardArray[boardArray.length-1]).appendChild(document.getElementById(players[j].id));
+        document.getElementById(boardArray[boardArray.length-1]).appendChild(document.getElementById(players[i].id));
         /*make game restart=page reload*/
     document.getElementById("restart").addEventListener("click", function(){
         window.location.href = window.location.href;
@@ -96,12 +154,12 @@ document.getElementById("button").addEventListener("click", function(){
     }
     /*if not win this happens*/
     else{
-    document.getElementById(boardArray[players[j].position+increment]).appendChild(document.getElementById(players[j].id));
-    players[j].position=players[j].position+increment;
+    document.getElementById(boardArray[players[i].position+increment]).appendChild(document.getElementById(players[i].id));
+    players[i].position=players[i].position+increment;
     /*not fully working here. i need to put it back to arr[0]*/
-    if(j==i-1){j=-1}
-    document.getElementById("whoIsPlaying").innerHTML=`<p>${players[j+1].name}, your turn</p>`
-    j++  
+    if(i==players.length-1){i=-1}
+    document.getElementById("whoIsPlaying").innerHTML=`<p>${players[i+1].name}, your turn</p>`
+    i++  
 }});
 
 ;
